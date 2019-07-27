151707019905fe343a51443792db4928fcb3918e
cayden <caydenakins@gmail.com>
2019-07-27 13:28:18 -0500

    original repository

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+All notable changes to "Build an Angular Instant Search Component” will be documented in this file.
+
+## 1.0.0
+
+### Added
+
+### Changed
+
+### Removed
---
+++ lessons/03-prevent-unnecessary-requests-in-angular-2-with-distinctuntilchanged/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { WikipediaSearchService } from './wikipedia-search.service';
+import { Subject } from 'rxjs/Subject';
+//application wide shared Rx operators
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/mergeMap';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  items:Array<string>;
+  term$ = new Subject<string>();
+  constructor(private service:WikipediaSearchService) {
+    
+    this.term$
+        .debounceTime(400)
+        .distinctUntilChanged()
+        .subscribe(term => this.search(term))
+  }
+
+  search(term: string) {
+    this.service.search(term)
+                .subscribe(results => this.items = results);
+  }
+}
